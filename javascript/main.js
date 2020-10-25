e6f0c489f929027a61c3317fdc5cce3088ca67d9
Brian <Brian@Brians-MacBook-Air.local>
2020-10-25 02:16:12 -0500

    finished up changes

--- javascript/main.js
+++ javascript/main.js
@@ -1,13 +1,18 @@
 const btn = document.getElementById('menu-btn');
 const dropDown = document.getElementById('dropdown');
+const logo = document.getElementById('logo')
 
 btn.addEventListener('click', toggleNav);
 
 function toggleNav(){
   if(dropdown.style.display === 'grid'){
     dropdown.style.display = 'none';
+    logo.style.visibility = 'visible'
+
   }else{
     dropdown.style.display = 'grid';
+    logo.style.visibility = 'hidden'
   }
   console.log('you clicked me')
 }
+
